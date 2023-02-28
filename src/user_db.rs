18b6ad26cf839f661d0d1e28e0da34b1f69f44be
stalker57241 <stalker3204@yandex.com>
2023-02-28 11:23:06 +0300

    Refactoring + added user system

--- src/user_db.rs
+++ src/user_db.rs
@@ -23,6 +23,9 @@ impl UserDB {
 
         return product_db;
     }
+    pub fn get_uid_by_title(&self, title: String) -> usize {
+        return self.connection.query_row("SELECT uid FROM `Users` WHERE name=?", [title, ], |row| {return row.get(0)}).unwrap();
+    }
 }
 
 impl Clone for UserDB {
@@ -33,14 +36,14 @@ impl Clone for UserDB {
 
 impl UserDBTrait for UserDB {
     fn add_user(&mut self, username: String, password: String) {
-        self.connection.execute("INSERT INTO `Users` (name, password, balance, banned, is_admin) VALUES (?1, ?2, ?3, ?4, ?5)", (username, password, 0.0f32, 0, 0)).unwrap();
+        self.connection.execute("INSERT INTO `Users` (name, password, balance, banned, is_admin) VALUES (?1, ?2, ?3, ?4, ?5);", (username, password, 0.0f32, 0, 0)).unwrap();
     }
 
     fn ban_user(&mut self, username: String) {
-        self.connection.execute("INSERT OR REPLACE INTO `Users` (name, banned) VALUES (?1, ?2)", (username, 1)).unwrap();
+        self.connection.execute("INSERT OR REPLACE INTO `Users` (name, banned) VALUES (?1, ?2);", (username, 1)).unwrap();
     }
     fn is_user_exists(&self, username: String) -> bool {
-        match self.connection.query_row("SELECT uid FROM `Users` WHER name=?", [username,], |row| {
+        match self.connection.query_row("SELECT uid FROM `Users` WHERE name=?;", [username,], |row| {
             return row.get::<usize, u32>(0)
         }) {
             Ok(_) => return true,
@@ -48,11 +51,11 @@ impl UserDBTrait for UserDB {
         };
     }
 
-    fn get_password(&mut self, username: String) -> String {
+    fn get_password(&self, username: String) -> String {
         return
             self.connection
-                .query_row("SELECT password FROM `Users` WHERE name=?", [username, ], |row| {
-                    return row.get(0);
+                .query_row("SELECT password FROM `Users` WHERE name=?;", [username, ], |row| {
+                    return row.get::<usize, String>(0usize);
                 }).unwrap();
     }
 
@@ -60,13 +63,30 @@ impl UserDBTrait for UserDB {
         return
             self
                 .connection
-                .query_row("SELECT balance FROM `Users` WHERE name=?", [username,], |row| row.get(0))
+                .query_row("SELECT balance FROM `Users` WHERE uid=?;", [self.get_uid_by_title(username.clone()),], |row| row.get::<usize, f32>(0))
                 .unwrap();
     }
     fn set_balance(&mut self, username: String, balance: f32) {
         self
             .connection
-            .execute("INSERT OR REPLACE INTO `Users` (name, balance) VALUES (?1, ?2)", (username, balance)).unwrap();
+            .execute("UPDATE `Users` SET balance=?2 WHERE uid=?1;", (self.get_uid_by_title(username), balance))
+            .unwrap();
+    }
+
+    fn set_admin(&mut self, username: String, admin: usize) {
+        self
+            .connection
+            .execute("UPDATE `Users` SET is_admin=?2 WHERE uid=?1;", (self.get_uid_by_title(username), admin))
+            .unwrap();
+    }
+    fn get_admin(&self, username: String) -> usize {
+        return 
+            self
+                .connection
+                .query_row("SELECT is_admin FROM `Users` WHERE uid=?;", [self.get_uid_by_title(username),], |row| {
+                    return row.get(0);
+                })
+                .unwrap();
     }
 
 }
